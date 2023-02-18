118fad2dcc540aa564158903c0f480ae79091f5d
MUHAMMED-SHAIKH-ZAHID <muhdshaikhzahid@gmail.com>
2023-02-18 10:43:56 +0530

    Home page done

--- src/Components/home/Social.jsx
+++ src/Components/home/Social.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="" className="home__social-icon" target="_black">
+        <a href="https://www.instagram.com/_shaikh_zahid/" className="home__social-icon" target="_black">
             <i className="uil uil-instagram"></i>
         </a>
-        <a href="" className="home__social-icon" target="_black">
-            <i className="uil uil-dribble"></i>
+        <a href="https://www.linkedin.com/in/muhammed-shaikh-zahid-u-155a98248/" className="home__social-icon" target="_black">
+            <i className="uil uil-dribbble"></i>
         </a>
-        <a href="" className="home__social-icon" target="_black">
+        <a href="https://github.com/MUHAMMED-SHAIKH-ZAHID" className="home__social-icon" target="_black">
             <i className="uil uil-github-alt"></i>
         </a>
     </div>
